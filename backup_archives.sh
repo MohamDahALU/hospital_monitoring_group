2c76dbb442e235d8ce6d87f96e952203acb3312a
Hamse <h.jama@alustudent.com>
2024-10-15 15:50:16 +0200

    debug backup

--- backup_archives.sh
+++ backup_archives.sh
@@ -1,4 +1,4 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 mkdir archived_logs_group30
 mv heart_rate_log.txt_* archived_logs_group30/
 scp -r archived_logs_group30/ d35d089e5e53@d35d089e5e53.7b3ea1d9.alu-cod.online:/home/
